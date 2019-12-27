3e5bd22efa1bc60211c3f7aa4a88a159e5bdd31f
ash1969 <nikhil.sonksbs@gmail.com>
2019-12-28 03:23:10 +0530

    Basic UI Setup

---
+++ WPF ZooManager/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WPF_ZooManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["WPF_ZooManager.Properties.Settings.PanjutorialsDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            ShowZoos();
+            ShowAllAnimals();
+        }
+
+        private void ShowAllAnimals()
+        {
+            string query = "select * from Animal";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+            using (sqlDataAdapter)
+            {
+                DataTable animalTable = new DataTable();
+                sqlDataAdapter.Fill(animalTable);
+
+                listAllAnimals.DisplayMemberPath = "Name";
+                listAllAnimals.SelectedValuePath = "Id";
+                listAllAnimals.ItemsSource = animalTable.DefaultView;
+            }
+
+        }
+
+        private void ShowZoos()
+        {
+            string query = "select * from Zoo";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+            using (sqlDataAdapter)
+            {
+                DataTable zooTable = new DataTable();
+                sqlDataAdapter.Fill(zooTable);
+
+                listZoos.DisplayMemberPath = "Location";
+                listZoos.SelectedValuePath = "Id";
+                listZoos.ItemsSource = zooTable.DefaultView;
+            }
+
+        }
+
+        private void ShowAssociatedAnimals()
+        {
+            string query = "select * from Animal a inner join ZooAnimal " +
+                "za on za.AnimalId = a.Id where za.ZooId = @ZooId";
+
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+            using (sqlDataAdapter)
+            {
+                sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                
+                DataTable animalTable = new DataTable();
+                sqlDataAdapter.Fill(animalTable);
+
+                listAssociatedAnimals.DisplayMemberPath = "Name";
+                listAssociatedAnimals.SelectedValuePath = "Id";
+                listAssociatedAnimals.ItemsSource = animalTable.DefaultView;
+            }
+
+        }
+
+        private void listZoos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowAssociatedAnimals();
+        }
+    }
+}
